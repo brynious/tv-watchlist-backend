a04f65ff57f03a02534858a38dfbc46cd09f21cc
Bryn McIvor <brynmcivor1@gmail.com>
2021-08-08 12:40:27 +1200

    first commit

---
+++ models/TvSeries.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const TvSeriesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  tmdb_id: {
+    type: String,
+    required: true,
+  },
+  overview: {
+    type: String,
+  },
+  first_air_date: {
+    type: "String",
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now,
+  },
+  backdrop_path: {
+    type: String,
+    required: true,
+  },
+  poster_path: {
+    type: String,
+    required: true,
+  },
+  watch_status: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = TvSeries = mongoose.model("series", TvSeriesSchema);
---
+++ routes/api/series.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+
+// Load TvSeries model
+const TvSeries = require("../../models/TvSeries");
+
+// @route GET api/series/test
+// @description tests series route
+// @access Public
+router.get("/test", (req, res) => res.send("tv series route testing!"));
+
+// @route GET api/series/status=:status
+// @description Get all series that match status
+// @access Public
+router.get("/status=:status", (req, res) => {
+  TvSeries.find({ watch_status: `${req.params.status}` })
+    .then(series => res.json(series))
+    .catch(err =>
+      res
+        .status(404)
+        .json({ noseriesfound: `No Series of status ${status} found` })
+    );
+});
+
+// @route GET api/series
+// @description Get all series
+// @access Public
+router.get("/", (req, res) => {
+  TvSeries.find()
+    .then(series => res.json(series))
+    .catch(err => res.status(404).json({ noseriesfound: "No Series found" }));
+});
+
+// @route GET api/series/:id
+// @description Get single series by id
+// @access Public
+router.get("/:id", (req, res) => {
+  TvSeries.findById(req.params.id)
+    .then(series => res.json(series))
+    .catch(err => res.status(404).json({ noseriesfound: "No Series found" }));
+});
+
+// @route GET api/series
+// @description add/save series
+// @access Public
+router.post("/", (req, res) => {
+  TvSeries.create(req.body)
+    .then(series => res.json({ msg: "TV series added successfully" }))
+    .catch(err =>
+      res.status(400).json({ error: "Unable to add this TV series" })
+    );
+});
+
+// @route GET api/series/:id
+// @description Update series
+// @access Public
+router.put("/:id", (req, res) => {
+  TvSeries.findByIdAndUpdate(req.params.id, req.body)
+    .then(series => res.json({ msg: "Updated successfully" }))
+    .catch(err =>
+      res.status(400).json({ error: "Unable to update the Database" })
+    );
+});
+
+// @route GET api/series/:id
+// @description Delete series by id
+// @access Public
+router.delete("/:id", (req, res) => {
+  TvSeries.findByIdAndRemove(req.params.id, req.body)
+    .then(series => res.json({ mgs: "Series entry deleted successfully" }))
+    .catch(err => res.status(404).json({ error: "No such TV series" }));
+});
+
+module.exports = router;
