356afb10bf7464ad7a4230c43a0845b30377ee95
Bryn McIvor <brynmcivor1@gmail.com>
2021-08-10 00:06:26 +1200

    Update series.js
    comment out unnecessary views
    switch to using deleteMany() instead of findByIdAndRemove()
    switch to upsert capable findOndAndUpdate() function instead of create()

--- routes/api/series.js
+++ routes/api/series.js
@@ -7,7 +7,7 @@ const TvSeries = require("../../models/TvSeries");
 // @route GET api/series/test
 // @description tests series route
 // @access Public
-router.get("/test", (req, res) => res.send("tv series route testing!"));
+// router.get("/test", (req, res) => res.send("tv series route testing!"));
 
 // @route GET api/series/status=:status
 // @description Get all series that match status
@@ -34,39 +34,49 @@ router.get("/", (req, res) => {
 // @route GET api/series/:id
 // @description Get single series by id
 // @access Public
-router.get("/:id", (req, res) => {
-  TvSeries.findById(req.params.id)
-    .then(series => res.json(series))
-    .catch(err => res.status(404).json({ noseriesfound: "No Series found" }));
-});
+// router.get("/:id", (req, res) => {
+//   TvSeries.findById(req.params.id)
+//     .then(series => res.json(series))
+//     .catch(err => res.status(404).json({ noseriesfound: "No Series found" }));
+// });
 
 // @route GET api/series
 // @description add/save series
 // @access Public
 router.post("/", (req, res) => {
-  TvSeries.create(req.body)
-    .then(series => res.json({ msg: "TV series added successfully" }))
-    .catch(err =>
-      res.status(400).json({ error: "Unable to add this TV series" })
-    );
+  const query = { tmdb_id: req.body.tmdb_id };
+  const update = req.body;
+  const options = { upsert: true, new: true };
+
+  TvSeries.findOneAndUpdate(query, update, options)
+    .then(series => res.json({ msg: "TV series add successfully" }))
+    .catch(err => {
+      res.status(400).json({ error: "Unable to add series to watchlist" });
+    });
+
+  // TvSeries.create(req.body)
+  //   .then(series => res.json({ msg: "TV series added successfully" }))
+  //   .catch(err =>
+  //     res.status(400).json({ error: "Unable to add this TV series" })
+  //   );
 });
 
 // @route GET api/series/:id
 // @description Update series
 // @access Public
-router.put("/:id", (req, res) => {
-  TvSeries.findByIdAndUpdate(req.params.id, req.body)
-    .then(series => res.json({ msg: "Updated successfully" }))
-    .catch(err =>
-      res.status(400).json({ error: "Unable to update the Database" })
-    );
-});
+// router.put("/:id", (req, res) => {
+//   TvSeries.findByIdAndUpdate(req.params.id, req.body)
+//     .then(series => res.json({ msg: "Updated successfully" }))
+//     .catch(err =>
+//       res.status(400).json({ error: "Unable to update the Database" })
+//     );
+// });
 
 // @route GET api/series/:id
 // @description Delete series by id
 // @access Public
 router.delete("/:id", (req, res) => {
-  TvSeries.findByIdAndRemove(req.params.id, req.body)
+  TvSeries.deleteMany({ tmdb_id: req.params.id })
     .then(series => res.json({ mgs: "Series entry deleted successfully" }))
     .catch(err => res.status(404).json({ error: "No such TV series" }));
 });
